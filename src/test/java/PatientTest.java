c760e9e4b9d01771da54fce784f7680cb0634a48
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2016-02-08 16:53:21 +0200

    progress

--- src/test/java/PatientTest.java
+++ src/test/java/PatientTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.assertEquals;
 
+import ca.uhn.fhir.model.primitive.IdDt;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import org.junit.Test;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.dstu2.resource.Bundle;
@@ -14,13 +16,27 @@ public class PatientTest {
 
         IGenericClient client = ctx.newRestfulGenericClient(serverBase);
 
+        Patient patient = new Patient();
+        patient.addIdentifier().setSystem("urn:system").setValue("value-aidbox");
+        patient.addName().addFamily("Aidbox").addGiven("John");
+
+        MethodOutcome outcome = client.create()
+                .resource(patient)
+                .conditional()
+                .where(Patient.IDENTIFIER.exactly().systemAndIdentifier("urn:system", "value-aidbox"))
+                .execute();
+
+        Boolean created = outcome.getCreated();
+
+        IdDt id = (IdDt) outcome.getId();
+
         Bundle results = client
                 .search()
                 .forResource(Patient.class)
-                .where(Patient.FAMILY.matches().value("duck"))
+                .where(Patient.FAMILY.matches().value("Aidbox"))
                 .returnBundle(ca.uhn.fhir.model.dstu2.resource.Bundle.class)
                 .execute();
 
-        assertEquals("Must be 10 patients named 'duck'", 10, results.getEntry().size());
+        assertEquals("Must be 1 patient named 'Aidbox'", 1, results.getEntry().size());
     }
 }
